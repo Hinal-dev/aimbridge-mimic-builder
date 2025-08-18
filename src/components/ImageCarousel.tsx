9ddfc0dd2271dae81e0e24faf9e6670e14697caf
Hinal-dev <hinal.thakrar@trinayatech.com>
2025-08-18 08:19:52 +0530

    changes

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -7,23 +7,23 @@ const ImageCarousel = () => {
   
   const images = [
     {
-      src: "/lovable-uploads/6b553e4e-7b6d-430a-9b29-8b9a5400bb14.png",
+      src: "/uploads/6b553e4e-7b6d-430a-9b29-8b9a5400bb14.png",
       alt: "Travelodge Hotel Exterior"
     },
     {
-      src: "/lovable-uploads/22e0a815-dede-4b3b-8502-ef7cfc194bfe.png",
+      src: "/uploads/22e0a815-dede-4b3b-8502-ef7cfc194bfe.png",
       alt: "Hotel Room Interior"
     },
     {
-      src: "/lovable-uploads/56a98ddb-5119-45e9-964d-f3002600f6ef.png",
+      src: "/uploads/56a98ddb-5119-45e9-964d-f3002600f6ef.png",
       alt: "Hotel Lobby and Reception"
     },
     {
-      src: "/lovable-uploads/9ef73cb6-22d2-458e-9591-ee55ef5a33ef.png",
+      src: "/uploads/9ef73cb6-22d2-458e-9591-ee55ef5a33ef.png",
       alt: "Super 8 Hotel Exterior"
     },
     {
-      src: "/lovable-uploads/5e6b71ee-13a3-445a-8bd3-455976a8e442.png",
+      src: "/uploads/5e6b71ee-13a3-445a-8bd3-455976a8e442.png",
       alt: "Hotel Atrium and Lounge"
     }
   ];
